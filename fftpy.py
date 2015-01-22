bfa73ca22ccb85737ad04bbe2c525ba98dc24e8b
George Ainsworth <georgeainsworthjr@gmail.com>
2015-01-21 22:48:06 -0200

    Mathscript and argparse integration completed

--- fftpy.py
+++ fftpy.py
@@ -7,7 +7,8 @@ import matplotlib
 import subprocess
 
 def main():
-    # Parse command-line arguments
+
+    # parse command-line arguments
     parser = argparse.ArgumentParser(usage=__doc__)
     parser.add_argument('--order', type=int, default=2, help='number of points')
     # parser.add_argument('--guess', type=float, default=1.0, help='initial guess')
@@ -16,10 +17,31 @@ def main():
 
     np = args.order
 
-    subprocess.check_output(['MathematicaScript','-script','teste.m'])
+    # o mathematica usara o script teste e gerara um arquivo com a funcao a ser transformada
+    # o arquivo ascii data.txt sera produzido com os pontos (x,y) da funcao descrita em teste.m
+    subprocess.check_output(['MathematicaScript', '-script', 'teste.m', str(np)])
 
-    t = numpy.linspace(0, 50e-6, num=25)
-    print(t)
+    # conta a quantidade de samplings do sinal
+    fdata   = open('data.txt', 'r')
+    nlinhas = 0
+
+    for i in fdata:
+        nlinhas = nlinhas + 1
+
+    data = numpy.zeros((nlinhas,2), float)
+
+    count = 0
+    fdata.seek(0) # o ponteiro do arquivo precisa ser recolocado no inicio
+
+    # cada linha é lida como uma cadeia de caracteres
+    for linha in fdata:
+        linha         = linha.strip() #as linhas em branco são removidos
+        colunas       = linha.split() #os numeros sao separados levando em conta o espaco em branco entre eles
+        data[count,0] = float(colunas[0])
+        data[count,1] = float(colunas[1])
+        count         = count + 1
+
+    print(data)
 
 if __name__ == '__main__':
     main()
--- teste.m
+++ teste.m
@@ -1,5 +1,7 @@
 f[t_] := 129534*(Exp[-t*(2*^06/68.2)] - Exp[-t*(2*^06/0.405)])
-tval = Table[N[t], {t, 0, 50*^-6, 50*^-6/25}]
-fval = Table[f[t], {t, 0, 50*^-6, 50*^-6/25}]
+Export["cmd.txt", $CommandLine,"Table"];
+ns    =ToExpression[$CommandLine[[8]]]
+tval = Table[N[t], {t, 0, 50*^-6, 50*^-6/ns}]
+fval = Table[f[t], {t, 0, 50*^-6, 50*^-6/ns}]
 data = MapThread[{#1, #2} &, {tval, fval}] (*junta as colunas de dadosa *)
 Export["data.txt", data, "Table"];
